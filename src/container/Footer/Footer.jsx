0f32937f2bff47d4964df8fa115236f8c981a3a0
Masterzerr <alejerry24@gmail.com>
2023-08-15 23:46:21 +0800

    final

--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -1,10 +1,47 @@
 import React from 'react';
 
+import { images } from '../../constants';
+import { FiFacebook, FiTwitter, FiInstagram } from 'react-icons/fi';
+import { FooterOverlay, Newsletter } from '../../components';
 import './Footer.css';
 
 const Footer = () => (
-  <div>
-    Footer
+  <div className="app__footer section__padding">
+    <FooterOverlay />
+    <Newsletter />
+
+    <div className="app__footer-links">
+      <div className="app__footer-links_contact">
+        <h1 className="app__footer-headtext">Contact Us</h1>
+        <p className="p__opensans">JA Clarin St., Tagbilaran City, Bohol Philippines</p>
+        <p className="p__opensans">+0912 345 6788</p>
+        <p className="p__opensans">+0998 765 4321</p>
+      </div>
+      <div className="app__footer-links_logo">
+        <img src={images.gericht} alt="footer__logo" />
+        <p className="p__opensans">
+          Lorem Ipsum Dolor Sit Amet, Consectetur Adipiscing Elit
+        </p>
+        <img src={images.spoon} alt="spoon" className='spoon__img' style={{ marginTop: 15 }} />
+        <div className="app__footer-links_icons">
+          <FiFacebook />
+          <FiTwitter />
+          <FiInstagram />
+        </div>
+      </div>
+      <div className="app__footer-links_work">
+        <h1 className="app__footer-headtext">Working Hours</h1>
+        <p className="p__opensans">Monday-Friday</p>
+        <p className="p__opensans">7:00AM - 5:00PM</p>
+        <p className="p__opensans">Monday-Friday</p>
+        <p className="p__opensans">9:00AM - 8:00PM</p>
+      </div>
+    </div>
+    <div className="footer__copyright">
+      <p className="p__opensans">
+        All rights reserved
+      </p>
+    </div>
   </div>
 );
 
